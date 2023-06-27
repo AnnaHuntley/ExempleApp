2c9e85c317d6d14d344eb4a571720d818fa76920
Anna Samus <annapn32@gmail.com>
2023-06-27 21:38:10 +0100

    added continuous delivery3

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
-sudo apt update $$ sudo apt install nodejs npm
-#Install pm2 wich is a production process manager for Node.js with s built-inload balancer.
+
+sudo apt update && sudo apt install nodejs npm
+
+# Install pm2, which is a production process manager for Node.js with a built-in load balancer.
 sudo npm install -g pm2
-#change directory into folder where application is downloded
-cd ExampleApplicatio/
-#Install application dependencies
+
+# Change directory to the folder where the application is downloaded.
+cd ExampleApplication/
+
+# Install application dependencies.
 npm install
-#Start the application with the process name example_app using pm2
-pm2 start ./bin/www ---name example_app
+
+# Start the application with the process name "example_app" using pm2.
+pm2 start ./bin/www --name example_app
